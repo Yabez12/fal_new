e0351a6a46b2a45e817a84ad7e2ea763ac5f6bd5
Yabez12 <108053607+Yabez12@users.noreply.github.com>
2022-07-02 12:25:46 +0530

    Add files via upload

---
+++ src.js
@@ -0,0 +1,198 @@
+let hideEle = () => {
+        const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'block';
+        e1[0].style.display='none';
+        e2[0].style.display='none';
+        e3[0].style.display='none';
+        e4[0].style.display='none';
+        e5[0].style.display='none';
+        e6[0].style.display='none';
+        e7[0].style.display='none';
+        e8[0].style.display='none';
+
+        
+    }
+
+let hie=()=>{
+   const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+    e1[0].style.display='block';
+    e2[0].style.display='none';
+        e3[0].style.display='none';
+        e4[0].style.display='none';
+        e5[0].style.display='none';
+        e6[0].style.display='none';
+        e7[0].style.display='none';
+        e8[0].style.display='none';
+        ele[0].style.display='none';
+}
+
+let hie1=()=>{
+            const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'none';
+        e1[0].style.display='none';
+        e2[0].style.display='block';
+        e3[0].style.display='none';
+        e4[0].style.display='none';
+        e5[0].style.display='none';
+        e6[0].style.display='none';
+        e7[0].style.display='none';
+        e8[0].style.display='none';
+
+}
+
+let hie2=()=>{
+            const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'none';
+        e1[0].style.display='none';
+        e2[0].style.display='none';
+        e3[0].style.display='block';
+        e4[0].style.display='none';
+        e5[0].style.display='none';
+        e6[0].style.display='none';
+        e7[0].style.display='none';
+        e8[0].style.display='none';
+
+}
+
+let hie3=()=>{
+            const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'none';
+        e1[0].style.display='none';
+        e2[0].style.display='none';
+        e3[0].style.display='none';
+        e4[0].style.display='block';
+        e5[0].style.display='none';
+        e6[0].style.display='none';
+        e7[0].style.display='none';
+        e8[0].style.display='none';
+
+}
+
+let hie4=()=>{
+            const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'none';
+        e1[0].style.display='none';
+        e2[0].style.display='none';
+        e3[0].style.display='none';
+        e4[0].style.display='none';
+        e5[0].style.display='block';
+        e6[0].style.display='none';
+        e7[0].style.display='none';
+        e8[0].style.display='none';
+
+}
+
+let hie5=()=>{
+            const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'none';
+        e1[0].style.display='none';
+        e2[0].style.display='none';
+        e3[0].style.display='none';
+        e4[0].style.display='none';
+        e5[0].style.display='none';
+        e6[0].style.display='block';
+        e7[0].style.display='none';
+        e8[0].style.display='none';
+
+}
+
+let hie6=()=>{
+            const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'none';
+        e1[0].style.display='none';
+        e2[0].style.display='none';
+        e3[0].style.display='none';
+        e4[0].style.display='none';
+        e5[0].style.display='none';
+        e6[0].style.display='none';
+        e7[0].style.display='block';
+        e8[0].style.display='none';
+
+}
+
+let hie7=()=>{
+            const e8=document.getElementsByClassName('container9');
+        const e7=document.getElementsByClassName('container8');
+        const e6=document.getElementsByClassName('container7');
+        const e5=document.getElementsByClassName('container6');   
+       const e4=document.getElementsByClassName('container5');
+       const e3=document.getElementsByClassName('container4');
+        const e2=document.getElementsByClassName('container3');
+        const ele = document.getElementsByClassName('container2');
+        const e1=document.getElementsByClassName('container1');
+        ele[0].style.display = 'none';
+        e1[0].style.display='none';
+        e2[0].style.display='none';
+        e3[0].style.display='none';
+        e4[0].style.display='none';
+        e5[0].style.display='none';
+        e6[0].style.display='none';
+        e7[0].style.display='none';
+        e8[0].style.display='block';
+
+}
+
